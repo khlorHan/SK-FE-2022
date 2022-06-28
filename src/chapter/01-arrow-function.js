22d0c062e71108c4c4b45eff1fc2d6c33c5405f7
yamoo9 <yamoo9@naver.com>
2022-06-28 11:31:35 +0900

    learn. 화살표 함수, 템플릿 리터럴

--- src/chapter/01-arrow-function.js
+++ src/chapter/01-arrow-function.js
@@ -2,12 +2,21 @@
 /* ECMAScript NEXT를 사용해 함수를 작성합니다.                                       */
 /* -------------------------------------------------------------------------- */
 
-function fibonacci(n) {
-  if (n === 0) return 0;
-  if (n === 1) return 1;
+// 함수 선언 → 화살표 함수 식
+const fibonacci = (n) => {
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
   return fibonacci(n - 1) + fibonacci(n - 2);
-}
-
-var numberWithComma = function (n) {
-  return n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 };
+
+console.log(fibonacci(7));
+
+// 일반 함수 식 → 화살표 함수 식
+const numberWithComma = (n) =>
+  n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+
+console.log(numberWithComma(9800));
--- src/chapter/02.1-template-literal.js
+++ src/chapter/02.1-template-literal.js
@@ -3,20 +3,29 @@
 /* -------------------------------------------------------------------------- */
 
 function renderCard(card) {
-  return (
-    '<div class="card">\
-      <img src="' + card.image.src +'" class="card-img-top" alt="' + card.image.alt + '" />\
-      <div class="card-body">\
-        <h5 class="card-title">' + card.title + '</h5>\
-        <p class="card-text">' + card.description + '</p>\
-        <a href="' + card.link.href + '" class="btn btn-primary">' + card.link.text + '</a>\
-      </div>\
-    </div>\
-    '
-  );
+  return `
+  <div class="card">
+    <img src="${card.image.src}" class="card-img-top" alt="${card.image.alt}" />
+    <div class="card-body">
+      <h5 class="card-title">${card.title}</h5>
+      <p class="card-text">${card.description}</p>
+      <a href="${card.link.href}" class="btn btn-primary">${card.link.text}</a>      
+    </div>    
+  </div>
+  `;
 }
 
-function removeSpaceString(string) {
-  return string.replace(/\s+/g, '');
-}
+const rendered = renderCard({
+  title: 'React 펀드멘탈',
+  description: 'React 애플리케이션 및 라이브러리를 빌드하는 데 필요한 기본 개념을 배웁니다.',
+  image: {
+    src: 'react-fundamentals.webp',
+    alt: 'React Fundamentals',
+  },
+  link: {
+    href: '/react-fundamentals',
+    text: 'React fundamentals',
+  },
+});
 
+console.log(rendered);
