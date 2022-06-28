7ede3a97378a12c53f464acfc4559ad963d8e426
yamoo9 <yamoo9@naver.com>
2022-06-28 13:37:03 +0900

    learn. 구조 분해 할당 (객체, 배열)

--- src/chapter/06-destructing-assignment.js
+++ src/chapter/06-destructing-assignment.js
@@ -2,7 +2,7 @@
 /* ECMAScript NEXT를 사용해 함수를 작성합니다.                                       */
 /* -------------------------------------------------------------------------- */
 
-var state = Object.freeze({
+const state = Object.freeze({
   loading: false,
   error: null,
   data: [
@@ -13,10 +13,21 @@ var state = Object.freeze({
 });
 
 // 객체 속성과 일치하는 변수 선언 후, 속성 값 할당
-var loading = state.loading;
-var error = state.error;
-var data = state.data;
+// let loading = state.loading;
+// const error = state.error;
+// const data = state.data;
+
+const { loading, error, data } = state; // { loading, error, data }
+
+console.log(loading);
+console.log(error);
+console.log(data);
 
 // 배열의 첫번째 아이템, 나머지 집합 추출
-var firstData = data[0];
-var restData = data.slice(1);
+// var firstData = data[0];
+// var restData = data.slice(1);
+
+const [firstData, ...restData] = data; // [firstData, secondData, lastData]
+
+console.log(firstData);
+console.log(restData);
