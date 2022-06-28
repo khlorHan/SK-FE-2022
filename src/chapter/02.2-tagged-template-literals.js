06d55dd18afdd34d8396c1df19a65ffba4a7587a
yamoo9 <yamoo9@naver.com>
2022-06-28 11:48:57 +0900

    learn. 태그 템플릿 문법

--- src/chapter/02.2-tagged-template-literals.js
+++ src/chapter/02.2-tagged-template-literals.js
@@ -8,10 +8,49 @@ const node = {
   style: { cssText: '' },
 };
 
-const styled = () => {
+const styled = (styleRules, element) => {
   // 코드를 작성합니다.
+
+  let styleCode = styleRules.reduce(
+    (prevValue, currentValue) => (prevValue + currentValue).trim(),
+    ''
+  );
+
+  element.style.cssText = styleCode;
 };
 
-const $styled = () => {
-  // 코드를 작성합니다.
+// styled()
+// styled`
+// 	${document.body}
+// 	margin: 10px;
+// 	background: salmon;
+// `;
+
+// 일반 함수 식 (ES5, 2009-2014)
+// const $styled = function (element) {
+//   return function (styleRules) {
+//     let styleCode = styleRules.reduce(
+//       (prevValue, currentValue) => (prevValue + currentValue).trim(),
+//       ''
+//     );
+//     element.style.cssText = styleCode;
+//   };
+// };
+
+// 화살표 함수 식 (2015 ~)
+const $styled = (element) => (styleRules) => {
+  let styleCode = styleRules.reduce(
+    (prevValue, currentValue) => (prevValue + currentValue).trim(),
+    ''
+  );
+
+  // Side Effects
+  // Imperative Programming
+  element.style.cssText = styleCode;
+  // return undefined;
 };
+
+$styled(document.body)`
+  margin: 10px;
+  background: salmon;
+`;
---
+++ src/index.js
@@ -0,0 +1,52 @@
+const node = {
+  name: 'figure',
+  className: 'tagged-template-literals',
+  style: { cssText: '' },
+};
+
+const styled = (styleRules, element) => {
+  // 코드를 작성합니다.
+
+  let styleCode = styleRules.reduce(
+    (prevValue, currentValue) => (prevValue + currentValue).trim(),
+    ''
+  );
+
+  element.style.cssText = styleCode;
+};
+
+// styled()
+// styled`
+// 	${document.body}
+// 	margin: 10px;
+// 	background: salmon;
+// `;
+
+// 일반 함수 식 (ES5, 2009-2014)
+// const $styled = function (element) {
+//   return function (styleRules) {
+//     let styleCode = styleRules.reduce(
+//       (prevValue, currentValue) => (prevValue + currentValue).trim(),
+//       ''
+//     );
+//     element.style.cssText = styleCode;
+//   };
+// };
+
+// 화살표 함수 식 (2015 ~)
+const $styled = (element) => (styleRules) => {
+  let styleCode = styleRules.reduce(
+    (prevValue, currentValue) => (prevValue + currentValue).trim(),
+    ''
+  );
+
+  // Side Effects
+  // Imperative Programming
+  element.style.cssText = styleCode;
+  // return undefined;
+};
+
+$styled(document.body)`
+  margin: 10px;
+  background: salmon;
+`;
