1acf08b5b8bb4137faa5afa3cd2ccd56417a25f6
yamoo9 <yamoo9@naver.com>
2022-06-29 15:51:57 +0900

    learn. React 리스트 렌더링(배열, 객체) & Vue 리스트 렌더링(v-for 디렉티브)

--- jsx-in-action/src/App.js
+++ jsx-in-action/src/App.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-unused-vars */
 
-import { createElement as h } from 'react';
+import { createElement as h, Fragment } from 'react';
 import { EmojiOops, PrettyPrintCode } from 'components';
+// import db from 'api/db.json';
 
 /* -------------------------------------------------------------------------- */
 
@@ -83,6 +84,27 @@ const ConditionalRendering = () => {
 
 /* -------------------------------------------------------------------------- */
 
+// CRA - ES Modules or CommonJS Modules
+const db = require('api/db.json');
+
+const { items } = db.navigation;
+
+// function
+const renderList = (list /* parameters */) =>
+  list.map(({ link, text }) => (
+    <li key={link}>
+      <a href={link}>{text}</a>
+    </li>
+  ));
+
+// functional component
+const RenderList = ({ list } /* props */) =>
+  list.map(({ link, text }) => (
+    <li key={link}>
+      <a href={link}>{text}</a>
+    </li>
+  ));
+
 // `api/db.json` 데이터에서 `navigation.items` 데이터를 화면에 출력해보세요.
 // `list` 매개 변수를 순환해 아이템 리스트를 반환하는 `renderList` 함수를 만들어 활용해봅니다.
 
@@ -93,21 +115,42 @@ const ListRendering = () => {
 
       {/* 배열 리스트 렌더링 */}
       <nav className="globalNavigation">
-        {/* 웹 표준을 준수해 비순차 목록을 화면에 출력해봅니다. */}
+        {/* 웹 표준을 준수해 비순차 목록(ul, unordered list)을 화면에 출력해봅니다. */}
         {/* renderList(list) 함수를 실행해 리스트 렌더링 처리해봅니다. */}
         <ul>
-          <li>리스트를 렌더링 해보세요.</li>
+          {/* <li>리스트를 렌더링 해보세요.</li> */}
+          <RenderList list={items} />
+          {/* {renderList(items)} */}
+          {/* {items.map(({ link, text }) => (
+            <li key={link}>
+              <a href={link}>{text}</a>
+            </li>
+          ))} */}
         </ul>
       </nav>
 
       {/* 객체 리스트 렌더링 */}
       <dl className="descriptionList">
-        {/* 웹 표준을 준수해 설명 목록을 화면에 출력해봅니다. */}
+        {Object.entries(db).map(([key, value]) => {
+          return (
+            <Fragment key={key}>
+              <dt>{key}</dt>
+              <dd>
+                {typeof value === 'string' ? (
+                  value
+                ) : (
+                  <PrettyPrintCode code={value} />
+                )}
+              </dd>
+            </Fragment>
+          );
+        })}
+        {/* 웹 표준을 준수해 설명 목록(dl, definition list > dt, dd)을 화면에 출력해봅니다. */}
         {/* `db` 객체의 "속성", "값" 쌍을 순환하여 리스트 렌더링 해보세요. */}
         {/* 값의 유형이 객체 또는 배열인 경우, 코드가 화면에 출력되도록 설정합니다. */}
         {/* `isArray`, `isObject` 유틸리티 함수를 만들어 활용하세요. */}
         {/* 코드를 화면에 출력할 때는 <PrettyPrintCode /> 컴포넌트를 활용합니다. (`code` 속성) */}
-        <PrettyPrintCode />
+        {/* <PrettyPrintCode code={db} /> */}
       </dl>
     </div>
   );
