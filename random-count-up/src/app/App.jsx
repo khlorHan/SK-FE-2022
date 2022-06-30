fa1d1420e48a4e8dffc90d93a932067f067d953d
yamoo9 <yamoo9@naver.com>
2022-06-30 11:51:12 +0900

    learn. stateless vs. stateful component & key prop (initialization)

--- random-count-up/src/app/App.jsx
+++ random-count-up/src/app/App.jsx
@@ -1,15 +1,45 @@
 import './App.css';
 import { Component } from 'react';
-import { RandomCountUp } from '@/components';
+import { RandomCountUp, Button } from '@/components';
+import { getRandom } from '@/utils';
 
+// stateful component
 class App extends Component {
+  state = {
+    reCountUpKey: 100,
+  };
+
   render() {
     return (
       <div className="app">
-        <RandomCountUp min={20} max={40} step={9} fps={15} />
+        <Button
+          lang="en"
+          style={{
+            position: 'fixed',
+            zIndex: 1000,
+            top: 20,
+            right: 20,
+          }}
+          onClick={this.handleReCountUp}
+        >
+          Re Count Up
+        </Button>
+        <RandomCountUp
+          key={this.state.reCountUpKey}
+          // min={10}
+          // max={90}
+          // step={9}
+          // fps={15}
+        />
       </div>
     );
   }
+
+  handleReCountUp = () => {
+    this.setState(({ reCountUpKey }) => ({
+      reCountUpKey: reCountUpKey + getRandom(100),
+    }));
+  };
 }
 
 export default App;
