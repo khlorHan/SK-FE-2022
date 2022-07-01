1a5cb62362eb1a2b6158218e9f651943bc0287a9
yamoo9 <yamoo9@naver.com>
2022-07-01 11:40:15 +0900

    learn. React 훅 학습
    - 이펙트, 레이아웃 이펙트 훅
    - eslint-plugin-react-hooks 설치 및 구성

--- stateful/src/components/RandomCountUp/RandomCountUp.hook.jsx
+++ stateful/src/components/RandomCountUp/RandomCountUp.hook.jsx
@@ -6,9 +6,11 @@ export default function RandomCountUp({ min, max, step, current }) {
   const [TARGET_COUNT] = useState(() => getRandomMinMax(min, max));
   useEffect(() => {
     document.title = `(${TARGET_COUNT}) ${document.title}`;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   /* -------------------------------------------------------------------------- */
+
   const [name, setName] = useState('Random Count Up');
   useEffect(() => {
     console.log('change name');
@@ -29,10 +31,20 @@ export default function RandomCountUp({ min, max, step, current }) {
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
 
+  // Promise
+  // useEffect(() => {
+  //   fetch('https://randomuser.me/api')
+  //     .then((response) => response.json())
+  //     .then((json) => setData(json))
+  //     .catch((error) => setError(error))
+  //     .finally(() => setLoading(false));
+  // }, []);
+
+  // Async Function
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch('https://randomuse.me/api');
+        const response = await fetch('https://randomuser.me/api');
         const json = await response.json();
         setData(json);
       } catch (error) {
@@ -51,22 +63,49 @@ export default function RandomCountUp({ min, max, step, current }) {
     data,
   });
 
+  /* -------------------------------------------------------------------------- */
+
+  useEffect(() => {
+    // 이벤트 구독
+    const clearIntervalId = setInterval(() => {
+      console.log('subscription');
+      console.log(Date.now());
+    }, 1000);
+
+    // 클린업(cleanup function) = 이펙트 함수가 반환하는 함수
+    // 이벤트 구독 취소
+    return () => {
+      console.log('cleanup');
+      clearInterval(clearIntervalId);
+    };
+  }, [count, name]);
+
   const handleIncrementCount = () => {
     setCount(count + step);
   };
 
   return (
-    <>
-      <button onClick={handleIncrementCount}>increment count</button>
-      <output>{count}</output>
-      <h1>{name}</h1>
-      <input
-        type="text"
-        onChange={(e) => {
-          setName(e.target.value);
+    <div style={{ margin: 30 }}>
+      <div
+        style={{
+          display: 'flex',
+          gap: 10,
+          marginBottom: 40,
         }}
-      />
-    </>
+      >
+        <button onClick={handleIncrementCount}>increment count</button>
+        <output>{count}</output>
+      </div>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <h1 style={{ marginTop: 4 }}>{name}</h1>
+      </div>
+    </div>
   );
 }
 
