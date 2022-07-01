76533ecff1aa2f4deb743562e21175f9880ea9c3
yamoo9 <yamoo9@naver.com>
2022-07-01 17:03:04 +0900

    learn. React Context with Custom Hooks → Management State (e.g Redux)

--- stateful/src/main.jsx
+++ stateful/src/main.jsx
@@ -3,15 +3,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ErrorBoundary } from '@/components';
+import { ThemeProvider } from '@/contexts';
 import '@/styles/globals.css';
 import App from '@/app/App.hook';
 
 const reactDomRoot = createRoot(document.getElementById('root'));
 
 reactDomRoot.render(
-  <>
-    <ErrorBoundary>
+  <ErrorBoundary>
+    <ThemeProvider theme="dark">
       <App />
-    </ErrorBoundary>
-  </>
+    </ThemeProvider>
+  </ErrorBoundary>
 );
